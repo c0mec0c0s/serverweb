c2a2f3b468d9d2ba623acbd702a96347d88eb82f
c0mec0c0s <jm.morenosoto@gmail.com>
2019-04-20 00:36:02 +0200

    update
    Se modifica web.h y se añade my.cnf (fichero necesario para habilitar que se pueda conectar a la base de datos desde fuera del servidor)

--- web.sh
+++ web.sh
@@ -27,3 +27,10 @@ sudo chown -R www-data: .
 
 sudo apt-get update
 
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update
+sudo apt-get install certbot python-certbot-apache -y
+
+sudo certbot --apache
