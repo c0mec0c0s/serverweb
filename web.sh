0e95647555afb2e8d1f4a54e14613ab76986bf8d
c0mec0c0s <jm.morenosoto@gmail.com>
2019-04-20 16:14:26 +0200

    Update web.sh

--- web.sh
+++ web.sh
@@ -8,8 +8,8 @@ sudo apt install apache2 -y
 # Step 2: Install PHP 7.1 and Related Modules
 
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php
-sudo apt update
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update -y
 
 sudo apt install php7.1 libapache2-mod-php7.1 php7.1-common php7.1-mbstring php7.1-xmlrpc php7.1-gd php7.1-xml php7.1-mysql php7.1-cli php7.1-mcrypt php7.1-zip php7.1-curl -y
 
@@ -25,12 +25,12 @@ sudo tar xzf latest.tar.gz
 sudo mv wordpress/* .
 sudo chown -R www-data: .
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository universe
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository universe -y
 sudo add-apt-repository ppa:certbot/certbot -y
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install certbot python-certbot-apache -y
 
 sudo certbot --apache
