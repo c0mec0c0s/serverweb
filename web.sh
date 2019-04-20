b763980f8fa9cdf43bd470fe5b79d5ff7e3e03f2
c0mec0c0s <jm.morenosoto@gmail.com>
2019-04-20 16:20:38 +0200

    Update web.sh

--- web.sh
+++ web.sh
@@ -27,10 +27,3 @@ sudo chown -R www-data: .
 
 sudo apt-get update -y
 
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository universe -y
-sudo add-apt-repository ppa:certbot/certbot -y
-sudo apt-get update -y
-sudo apt-get install certbot python-certbot-apache -y
-
-sudo certbot --apache
